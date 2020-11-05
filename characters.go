643141ab81f9d20c12bcadead44577eac607ee31
zhaxa <zhaxa65@gmail.com>
2020-11-06 01:46:34 +0600

    added inventory system added items started working on script reader fixed weapon system

--- characters.go
+++ characters.go
@@ -1,19 +1,65 @@
 package main
 
+import (
+	"fmt"
+)
+
 type Player struct {
 	playerName NamePicker
 	hp         int
+	weapon     Weapon
+	inventory  Inventory
+}
+
+type Inventory struct {
+	items []string
+	slots int
+}
+
+func (i *Inventory) addItem(item string) {
+	isAdded := false
+	for r := range i.items {
+		if i.items[r] == "" {
+			i.items[r] = item
+			isAdded = true
+			return
+		}
+	}
+	if isAdded {
+		fmt.Println("Item added successfully")
+	} else {
+		fmt.Println("Not enough space")
+	}
+
+}
+
+func (i *Inventory) removeItem(item string) {
+	isRemoved := false
+	for r := range i.items {
+		if i.items[r] == item {
+			i.items[r] = ""
+			isRemoved = true
+			return
+		}
+	}
+	if isRemoved {
+		fmt.Println("Item removed successfully")
+	} else {
+		fmt.Println("You dont have such item")
+	}
 }
 
 type Weapon struct {
 	weaponName   string
 	WeaponDamage int
-	isCapable    bool
+	isExists     bool
 }
 
 func (w *Weapon) Attack(enemy Enemy) Enemy {
-	if w.isCapable {
+	if w.isExists {
 		enemy.hp = enemy.hp - w.WeaponDamage
+	} else {
+		fmt.Println("You don't have a weapon!")
 	}
 	return enemy
 }
@@ -22,4 +68,9 @@ type Enemy struct {
 	enemyName   string
 	enemyDamage int
 	hp          int
+}
+
+func (e *Enemy) Attack(player Player) Player {
+	player.hp = player.hp - e.enemyDamage
+	return player
 }
--- script.go
+++ script.go
@@ -1,1 +1,17 @@
 package main
+
+type Script struct {
+	text   string
+	choice []string
+}
+
+type NPC struct {
+	name   string
+	script Script
+}
+
+type Reader struct{}
+
+func (r *Reader) Read() {
+
+}
