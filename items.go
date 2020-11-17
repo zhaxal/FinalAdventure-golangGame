231e3f713fbaac9e0b6c82a0b70232234558365f
Dair Alayev <dairalayev@gmail.com>
2020-11-17 12:17:50 +0600

    critical damage notifications added

--- items.go
+++ items.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 )
@@ -22,8 +23,10 @@ func (sb SwordBehavior) attack(enemy Enemy, attackDamage int) *Enemy {
 	chance := r1.Intn(4)
 	if chance == 1 {
 		enemy.hp = enemy.hp - int(float64(attackDamage)*1.5)
+		fmt.Println("Critical damage!")
 	} else {
 		enemy.hp = enemy.hp - attackDamage
+		fmt.Println("Successful attack")
 	}
 	return &enemy
 }
@@ -37,8 +40,10 @@ func (wb WandBehavior) attack(enemy Enemy, attackDamage int) *Enemy {
 	chance := r1.Intn(10)
 	if chance == 1 {
 		enemy.hp = enemy.hp - 2*attackDamage
+		fmt.Println("Critical damage!")
 	} else {
 		enemy.hp = enemy.hp - attackDamage
+		fmt.Println("Successful attack")
 	}
 	return &enemy
 }
@@ -52,10 +57,12 @@ func (bb BowBehavior) attack(enemy Enemy, attackDamage int) *Enemy {
 	chance := r1.Intn(5)
 	if chance == 1 {
 		enemy.hp = enemy.hp - 3*attackDamage
+		fmt.Println("Critical damage!")
 	} else if chance == 2 {
-
+		fmt.Println("You missed!")
 	} else {
 		enemy.hp = enemy.hp - attackDamage
+		fmt.Println("Successful attack")
 	}
 	return &enemy
 }
@@ -69,8 +76,10 @@ func (ab AxeBehavior) attack(enemy Enemy, attackDamage int) *Enemy {
 	chance := r1.Intn(8)
 	if chance == 1 {
 		enemy.hp = enemy.hp - int(float64(attackDamage)*2.5)
+		fmt.Println("Critical damage!")
 	} else {
 		enemy.hp = enemy.hp - attackDamage
+		fmt.Println("Successful attack")
 	}
 	return &enemy
 }
